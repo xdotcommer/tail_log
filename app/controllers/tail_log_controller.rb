c50712c124a8f36b0d544f05030267499962038c
Michael Cowden <mcowden@yahoo.com>
2008-11-23 21:06:59 -0600

    fixed misspelling

--- app/controllers/tail_log_controller.rb
+++ app/controllers/tail_log_controller.rb
@@ -1,7 +1,6 @@
 class TailLogController < ApplicationController
   layout nil
   
-  #RESTFUL??
   def index
     if params[:login] == TailLog.login && params[:password] == TailLog.password
       @logfiles = Dir.glob("#{RAILS_ROOT}/log/*.log")
@@ -9,7 +8,7 @@ class TailLogController < ApplicationController
       @refresh  = ! params[:refresh].blank?
       @login    = params[:login]
       @password = params[:password]
-      @current  = params[:logfile] || default_logfile(@logifiles) || @logfiles.first
+      @current  = params[:logfile] || default_logfile(@logfiles) || @logfiles.first
       @log      = TailLog::Log.new(@current, {:tail => @tail})
       @log.process_file
     else
